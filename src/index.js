8c3c51c3f5e320ff8f3b3b67e135cd3f80070a65
newdev03 <164910575+newdev03@users.noreply.github.com>
2025-02-12 09:58:54 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-let nome = "felipe";
-console.log(nome);
+var nome = "Luffy"
+let xp = 9000;
+
+if(xp <=1000){
+    console.log("O Pirata " + nome + " Tem a recompensa de 30 milhões de Berries ");
+}       else if (xp >= 1001 && xp <= 2000){
+    console.log("O Pirata " + nome + " Tem a recompensa de 100 milhões de Berries");
+}       else if (xp >= 2001 && xp <= 5000){
+    console.log("O Pirata " + " Tem a recompensa de 300 milhões de Berries");
+}       else if (xp >= 5001 && xp <= 7000){
+    console.log("O Pirata " + nome + " Tem a recompensa de 400 Milhões de Berries");
+}       else if (xp >= 7001 && xp <= 8000){
+    console.log("O Pirata " + nome + " Tem a recompensa de 500 Milhões de Berries");    
+}       else if (xp >= 8001 && xp <= 9000){
+    console.log("O Pirata " + nome + " Tem a recompensa de 1,5 Bilhão de Berries");
+}       else if (xp >= 9001 && xp <= 100001){
+    console.log("O Pirata " + nome + " Tem a Recompensa de 3 Bilhoões de Berries")
+}
